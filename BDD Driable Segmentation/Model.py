ee1336b835fab8d47e165ba0672cda65e3c26419
server2243 <ksb5180@gmail.com>
2020-03-05 00:41:52 +0900

    load error fixed

--- BDD Driable Segmentation/Model.py
+++ BDD Driable Segmentation/Model.py
@@ -5,17 +5,18 @@ from tensorflow.keras import Input, Model
 from tensorflow.keras import backend as K
 from tensorflow.keras import layers as L
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import models
 
 
-def acc(y_true, y_pred, smooth=1):
+def iou_acc(y_true, y_pred, smooth=1):
     intersection = K.sum(K.abs(y_true * y_pred), axis=[1, 2, 3])
     union = K.sum(y_true, [1, 2, 3]) + K.sum(y_pred, [1, 2, 3]) - intersection
     iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
     return iou
 
 
-def loss(y_true, y_pred, smooth=1):
-    return 1 - acc(y_true, y_pred, smooth)
+def iou_loss(y_true, y_pred, smooth=1):
+    return 1 - iou_acc(y_true, y_pred, smooth)
 
 
 def Conv_block(input, filter, kernel=3, last=False):
@@ -29,7 +30,7 @@ def Conv_block(input, filter, kernel=3, last=False):
 
 
 def Upsampling_block(input1, input2):
-    y = Conv_block(input2, input1.shape[-1], kernel=3)
+    y = Conv_block(input2, input1.shape[-1], kernel=1)
     x = L.UpSampling2D()(input1)
     x = L.Add()([x, y])
     return x
@@ -40,59 +41,48 @@ def Build():
     ch = 32
 
     d1 = Conv_block(tensor, ch)
-    d1 = Conv_block(d1, ch)
 
     m = L.MaxPool2D()(d1)
     d2 = Conv_block(m, ch * 2)
-    d2 = Conv_block(d2, ch * 2)
 
     m = L.MaxPool2D()(d2)
-    d3 = Conv_block(m, ch * 3)
-    d3 = Conv_block(d3, ch * 3)
+    d3 = Conv_block(m, ch * 4)
 
     m = L.MaxPool2D()(d3)
-    d4 = Conv_block(m, ch * 4)
-    d4 = Conv_block(d4, ch * 4)
+    d4 = Conv_block(m, ch * 6)
 
     m = L.MaxPool2D()(d4)
-    d5 = Conv_block(m, ch * 5)
-    d5 = Conv_block(d5, ch * 5)
+    d5 = Conv_block(m, ch * 8)
 
     m = L.MaxPool2D()(d5)
-    e = Conv_block(m, ch * 6)
-    e = Conv_block(e, ch * 6)
+    e = Conv_block(m, ch * 10)
+    e = Conv_block(e, ch * 10)
     m = Upsampling_block(e, d5)
 
-    u5 = Conv_block(m, ch * 5)
-    u5 = Conv_block(u5, ch * 5)
+    u5 = Conv_block(m, ch * 8)
     m = Upsampling_block(u5, d4)
 
-    u4 = Conv_block(m, ch * 4)
-    u4 = Conv_block(u4, ch * 4)
+    u4 = Conv_block(m, ch * 6)
     m = Upsampling_block(u4, d3)
 
-    u3 = Conv_block(m, ch * 3)
-    u3 = Conv_block(u3, ch * 3)
+    u3 = Conv_block(m, ch * 4)
     m = Upsampling_block(u3, d2)
 
     u2 = Conv_block(m, ch * 2)
-    u2 = Conv_block(u2, ch * 2)
     m = Upsampling_block(u2, d1)
 
     u1 = Conv_block(m, ch)
-    u1 = Conv_block(u1, ch)
 
-    e = Conv_block(u1, 3, last=True)
+    e = Conv_block(u1, 3, kernel=1, last=True)
 
     model = Model(tensor, e)
-    model.compile(Adam(epsilon=1e-5), loss, [acc])
+    model.compile(Adam(epsilon=1e-5), iou_loss, [iou_acc])
     model.summary()
     return model
 
 
 def LoadSavedModel():
     models_path = glob.glob('D:/Model/*.h5')
-    model = Build()
     if len(models_path):
         latest = max(models_path, key=os.path.getctime).replace('\\', '/')
         print('Loaded ' + str(latest))
@@ -104,12 +94,13 @@ def LoadSavedModel():
     if exist:
         ans = input('Load? ([y]/n)')
         if ans == 'n':
+            model = Build()
             print('Passed')
             return model
         else:
-            model.load_weights(filepath)
-            model.compile(Adam(epsilon=1e-5), loss, [acc])
+            model = models.load_model(filepath, {'iou_loss': iou_loss, 'iou_acc': iou_acc})
             print('Loaded Model')
             return model
     else:
+        model = Build()
         return model
--- BDD Driable Segmentation/Train.py
+++ BDD Driable Segmentation/Train.py
@@ -9,11 +9,11 @@ if gpus:
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
 callback = [
-    ReduceLROnPlateau(factor=0.2, patience=5, verbose=1),
-    ModelCheckpoint(path + 'model_{val_acc:.4f}_{acc:.4f}.h5')
+    ReduceLROnPlateau(factor=0.2, patience=6, verbose=1),
+    ModelCheckpoint(path + 'model_{val_iou_acc:.4f}_{iou_acc:.4f}.h5')
 ]
 
 tr_batch, te_batch = Data.Load()
 
 model = Model.LoadSavedModel()
-model.fit(tr_batch, epochs=20, verbose=1, callbacks=callback, validation_data=te_batch)
+model.fit(tr_batch, epochs=1, verbose=1, callbacks=callback, validation_data=te_batch)

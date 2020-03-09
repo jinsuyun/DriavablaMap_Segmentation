983717e95b927ca8ba66ba0828f46e56f752b0d1
server2243 <ksb5180@gmail.com>
2020-03-09 16:06:48 +0900

    threshold

--- Evaluate.py
+++ Evaluate.py
@@ -6,12 +6,13 @@ import Model
 model, epoch = Model.LoadSavedModel()
 imgs = glob.glob('C:/bdd100k/images/100k/test/*')
 np.random.shuffle(imgs)
+threshold = 0.9
 
 for path in imgs[:10]:
     img = cv.imread(path)
     img = cv.resize(img, (512, 288))
     predict = np.reshape(model.predict(np.expand_dims(img, axis=0) / 255), [288, 512, 3]) * 255
-    predict[predict <= (255 * 0.9)] = 0
+    predict[predict <= (255 * threshold)] = 0
     imgpred = cv.add(img, predict, dtype=cv.CV_8U)
     cv.imshow('imgpred', imgpred)
     cv.imshow('original', img)
